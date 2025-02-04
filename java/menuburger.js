a8b21d420f09338a8ac2fe06975eb58afb2a6184
isabelamihai <isabelaelenamihai@gmail.com>
2025-02-04 16:24:51 +0200

    Update

--- java/menuburger.js
+++ java/menuburger.js
@@ -1,6 +1,10 @@
-   const hamburger = document.getElementById('hamburger-menu');
-const navContainer = document.getElementById('nav-container');
+// Selectăm elementele necesare
+const hamburger = document.getElementById('hamburger-menu');
+const navContainer = document.querySelector('.nav-container'); // 
 
+// Adăugăm un event listener pentru click pe meniul hamburger
 hamburger.addEventListener('click', () => {
     navContainer.classList.toggle('active');
 });
+
+
